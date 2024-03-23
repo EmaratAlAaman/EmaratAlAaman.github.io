0b23dc7415b29af40a8a199ad9fa8536150456c1
Ali usman <aliusman.wiva@gmail.com>
2024-03-23 14:18:49 +0500

    InitialCommit

---
+++ public/js/custom.js
@@ -0,0 +1,238 @@
+$(window).on('load', function(){
+
+	"use strict";
+ 
+	
+	getMobileOperatingSystem()
+	
+	/* ========================================================== */
+	/*   Owl Carousel                                             */
+	/* ========================================================== */
+	
+	$('#owl1').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:false,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+	})
+
+	$('#owl2').owlCarousel({
+	    center:true,
+	    autoplay:true,
+	    loop:true,
+	    margin:40,
+	    nav:false,
+	    responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:1,
+	            nav:false
+	        },
+	        600:{
+	            items:2,
+	            nav:false
+	        },
+	        1000:{
+	            items:4,
+	            nav:false,
+	            loop:true
+	        }
+	    }
+	})
+ 
+	/* ========================================================== */
+	/*   Navigation Background Color                              */
+	/* ========================================================== */
+	
+	$(window).on('scroll', function() {
+		if($(this).scrollTop() > 600) {
+			$('#navbar-home').addClass('opaque');
+		} else {
+			$('#navbar-home').removeClass('opaque');
+		}
+	});
+ 
+	
+	/* ========================================================== */
+	/*   Hide Responsive Navigation On-Click                      */
+	/* ========================================================== */
+	
+	  $(".navbar-nav li a").on('click', function(event) {
+	    $(".navbar-collapse").collapse('hide');
+	  });
+
+	
+	/* ========================================================== */
+	/*   Navigation Color                                         */
+	/* ========================================================== */
+	
+	$('#navbar-collapse-02').onePageNav({
+		filter: ':not(.external)'
+	});
+
+
+	/* ========================================================== */
+	/*   SmoothScroll                                             */
+	/* ========================================================== */
+	
+	$(".nav li a, a.scrool").on('click', function(e) {
+		
+		var full_url = this.href;
+		var parts = full_url.split("#");
+		var trgt = parts[1];
+		var target_offset = $("#"+trgt).offset();
+		var target_top = target_offset.top;
+		
+		$('html,body').animate({scrollTop:target_top -70}, 1000);
+			return false;
+		
+	});
+
+
+	/* ========================================================== */
+	/*   Newsletter                                               */
+	/* ========================================================== */
+	
+	$('.newsletter_box .newsletter_form').each( function(){
+		var form = $(this);
+		//form.validate();
+		form.submit(function(e) {
+			if (!e.isDefaultPrevented()) {
+				jQuery.post(this.action,{
+					'email':$('input[name="nf_email"]').val(),
+				},function(data){
+					form.fadeOut('fast', function() {
+						$(this).siblings('p.newsletter_success_box').show();
+					});
+				});
+				e.preventDefault();
+			}
+		});
+	});	
+	
+
+	/* ========================================================== */
+	/*   Register                                                 */
+	/* ========================================================== */
+	
+	$('#register-form').each( function(){
+		var form = $(this);
+		//form.validate();
+		form.submit(function(e) {
+			if (!e.isDefaultPrevented()) {
+				jQuery.post(this.action,{
+					'names':$('input[name="register_names"]').val(),
+					'phone':$('input[name="register_phone"]').val(),
+					'email':$('input[name="register_email"]').val(),
+					'ticket':$('select[name="register_ticket"]').val(),
+				},function(data){
+					form.fadeOut('fast', function() {
+						$(this).siblings('p.register_success_box').show();
+					});
+				});
+				e.preventDefault();
+			}
+		});
+	})
+	
+	
+	/* ========================================================== */
+	/*   Contact                                                  */
+	/* ========================================================== */
+	$('#contact-form').each( function(){
+		var form = $(this);
+		//form.validate();
+		form.submit(function(e) {
+			if (!e.isDefaultPrevented()) {
+				jQuery.post(this.action,{
+					'names':$('input[name="contact_names"]').val(),
+					'subject':$('input[name="contact_subject"]').val(),
+					'email':$('input[name="contact_email"]').val(),
+					'phone':$('input[name="contact_phone"]').val(),
+					'message':$('textarea[name="contact_message"]').val(),
+				},function(data){
+					form.fadeOut('fast', function() {
+						$(this).siblings('p').show();
+					});
+				});
+				e.preventDefault();
+			}
+		});
+	})
+
+	
+});
+
+
+	/* ========================================================== */
+	/*   Popup-Gallery                                            */
+	/* ========================================================== */
+	$('.popup-gallery').find('a.popup1').magnificPopup({
+		type: 'image',
+		gallery: {
+		  enabled:true
+		}
+	}); 
+	
+	$('.popup-gallery').find('a.popup2').magnificPopup({
+		type: 'image',
+		gallery: {
+		  enabled:true
+		}
+	}); 
+ 
+	$('.popup-gallery').find('a.popup3').magnificPopup({
+		type: 'image',
+		gallery: {
+		  enabled:true
+		}
+	}); 
+ 
+	$('.popup-gallery').find('a.popup4').magnificPopup({
+		type: 'iframe',
+		gallery: {
+		  enabled:false
+		}
+	});  
+ 
+ 	/* ========================================================== */
+	/*   detect-device                                            */
+	/* ========================================================== */
+	
+	 
+	function getMobileOperatingSystem() {
+  		var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+		$("#download-header").hide();
+      	
+      	// Windows Phone must come first because its UA also contains "Android"
+    	if (/windows phone/i.test(userAgent)) {
+        	return "Windows Phone";
+    	}
+
+    	if (/android/i.test(userAgent)) {
+			$(".ios-app-store").hide();
+    		$("#play-store-header").removeClass('hide');
+    	    return "Android";
+    	}
+		
+    	// iOS detection from: http://stackoverflow.com/a/9039885/177710
+    	if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+	    	$(".play-store").hide();
+	    	$("#ios-app-store-header").removeClass('hide');
+    	    return "iOS";
+    	}
+
+		$("#download-header").show();
+	
+	    return "unknown";
+	}
